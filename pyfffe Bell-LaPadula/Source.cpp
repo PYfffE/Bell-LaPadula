12677c4a4f84fbf585ed65b70912657ade0c9a43
Алексей Панфилов <aapanfilov2@mail.ru>
2019-02-22 19:53:00 +0300

    first commit

--- pyfffe Bell-LaPadula/Source.cpp
+++ pyfffe Bell-LaPadula/Source.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
+class RootAccess
+{
+public:
+	RootAccess();
 
-	cout << "Hello world by pyfffe";
+private:
+
+};
+
+
+
+class MySubject
+{
+public:
+
+	MySubject(): StrLogin(""), StrPass("") { displayMessage(); };
+
+	void displayMessage() {
+		cout << "Пройдите авторизацию.\n";
+		setAuthentification();
+
+	}
+
+private:
+
+	void setAuthentification() {
+		setLogin();
+		setPassword();
+	}
+
+	void setLogin() {
+		cout << "Введите логин: ";
+		cin >> StrLogin;
+	}
+
+	void setPassword() {
+		cout << "Введите пароль: ";
+		cin >> StrPass;
+	}
+	
+	string StrLogin;
+	string StrPass;
+};
+
+
+
+
+class Object
+{
+	
+public:
+
+private:
+};
+
+
+
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+
+	MySubject UserTest;
+
 	
 	system("pause");
 	return 0;
